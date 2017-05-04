9c724f156eb60303edaf21a1730c0bb73ac98729
noctarius <me@noctarius.com>
2017-05-04 11:31:12 +0200

    Kotlin Frontend

---
+++ juber-anderson/src/test/kotlin/com/juber/anderson/JuberAndersonApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.juber.anderson
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class JuberAndersonApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
