2bc83c5c5ed3be2cbe5b40a079d6d9f5fb1bc86f
Viktor Gamov <gamussa@gmail.com>
2017-05-04 09:11:02 +0200

    dependencies and infra

---
+++ src/main/java/com/hazecast/copenhagen/Starter.java
@@ -0,0 +1,16 @@
+package com.hazecast.copenhagen;
+
+import com.hazelcast.jet.Jet;
+
+/**
+ * TODO
+ *
+ * @author Viktor Gamov on 5/4/17.
+ *         Twitter: @gamussa
+ * @since 0.0.1
+ */
+public class Starter {
+    public static void main(String[] args) {
+        Jet.newJetInstance();
+    }
+}
